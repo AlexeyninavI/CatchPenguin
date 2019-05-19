afe0011c965c5a17db98b1e2c4ab1a46edd4528a
BeYkeRYkt <beykerykt@gmail.com>
2019-05-20 04:20:16 +0700

    CatchPenguins: import "E3 2019" preview materials
    Change-Id: Iea76c6810783824a788e1b6acd977ed1d71d98ac

---
+++ Assets/Scripts/E3_2019_preview/RandomAnimatorSpeed.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimatorSpeed : MonoBehaviour
+{
+    public GameObject objectTarget;
+    public float minSpeed = 0.4f;
+    public float maxSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator animator = objectTarget.GetComponent<Animator>();
+        animator.speed = Random.Range(minSpeed, maxSpeed);
+    }
+}
