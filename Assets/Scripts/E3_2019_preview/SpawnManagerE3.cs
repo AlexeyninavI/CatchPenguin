d776b7751746112afdd89afe23795cdbbf8b01a5
BeYkeRYkt <beykerykt@gmail.com>
2019-05-23 01:04:05 +0700

    CatchPenguins: E3: add custom spawn manager
    Change-Id: Ic9ecb2045f0caffbdaa38f07abc6d17b52027e91

---
+++ Assets/Scripts/E3_2019_preview/SpawnManagerE3.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerE3 : MonoBehaviour
+{
+           
+    public GameObject PenguinV2;
+   
+    public float spawnTime = 3f;  // How long between each spawn.
+
+    public Transform[] spawnPoints = new Transform[4];
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        int targetNumber = Random.Range(0, spawnPoints.Length);
+        Transform targetSpawnPoint = spawnPoints[targetNumber];
+        Instantiate(PenguinV2, targetSpawnPoint.position, targetSpawnPoint.rotation);
+    }
+}
