0eb3e7e7a573db2085936f73eaa547075d64d37b
BeYkeRYkt <beykerykt@gmail.com>
2019-05-29 02:58:37 +0700

    CatchPenguins: penguins love to eat fish
    Change-Id: Ie8e4561597caada7c8ae2705e0cb14c222078b92

--- Assets/Scripts/PengCollision.cs
+++ Assets/Scripts/PengCollision.cs
@@ -12,8 +12,14 @@ public class PengCollision : MonoBehaviour
         if (collision.gameObject.name == "DeadZone")
         {
             Destroy(this.gameObject);
-
-
+        } else if (collision.gameObject.name == "fishBullet(Clone)")
+        {
+            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>(), true);
+            // типо жрет рыбу
+            Destroy(collision.gameObject);
+        } else if (collision.gameObject.tag == "Penguin")
+        {
+            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>(), true);
         }
     }
 }
