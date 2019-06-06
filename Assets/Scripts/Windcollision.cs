7d1f77a95cb9067a521f033a518d353c7e62c67f
BeYkeRYkt <beykerykt@gmail.com>
2019-06-06 17:06:04 +0700

    CatchPenguins: wind collision improvements
    Change-Id: I9fde1ebe96ee6462bab8641395b3d2c3bee85762

--- Assets/Scripts/Windcollision.cs
+++ Assets/Scripts/Windcollision.cs
@@ -7,89 +7,94 @@ public class Windcollision : MonoBehaviour
 {
     public Text messageText;
     public float force;
-    public float timeRemaining = 900f;
-    public TextMesh text;
+    public float timeRemaining = 5f;
+    private float tick = 0;
     bool wind = false;
     float hoverForce;
     int napravlen;
     public Transform bulletPrefab;
+
+    // text display
+    public int timeDisplayVisible = 2; // 2 seconds
+    private int tickDisplay = -1;
+
     void Start()
     {
-        InvokeRepeating("decreaseTimeRemaining", 1.0f, 1.0f);
-    
+        tick = timeRemaining;
+        InvokeRepeating("decreaseTick", 1.0f, 1.0f);
         Transform bullet = Instantiate(bulletPrefab) as Transform;
         Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GetComponent<Collider>());
     }
 
     void Update()
     {
-        decreaseTimeRemaining();
+        if(tickDisplay == 0)
+        {
+            tickDisplay = -1; // disable
+            messageText.text = "";
+        }
 
-        if (timeRemaining < 0 && wind == false)
+        if (tick < 0 && wind == false)
         {
             hoverForce = force;
-            timeRemaining = 900f;
+            tick = timeRemaining;
             napravlen = Random.Range(1, 4);
             wind = true;
+            tickDisplay = timeDisplayVisible;
             messageText.text = "Ветер дует";
         }
-        else if (timeRemaining < 0 && wind == true)
+        else if (tick < 0 && wind == true)
         {
             hoverForce = 0;
-            timeRemaining = 900f;
+            tick = timeRemaining * 2;
             wind = false;
+            tickDisplay = timeDisplayVisible;
             messageText.text = "Ветер не дует";
         }
-        
-            
-       
-
-        //GuiText.text = timeRemaining + " Seconds remaining!";
-
     }
 
-    void decreaseTimeRemaining()
+    void decreaseTick()
     {
-        timeRemaining--;
+        tick--;
+        if (tickDisplay > 0)
+        {
+            tickDisplay--;
+        }
     }
+
     void OnTriggerStay(Collider other)
     {
-
-        switch (napravlen)
+        if (wind)
         {
-            case 1:
-                {
-                    if (other.attachedRigidbody)
-                        other.attachedRigidbody.AddForce(Vector3.forward * hoverForce);
-                    
-                    break;
-                }
-            case 2:
-                {
-                    if (other.attachedRigidbody)
-                        other.attachedRigidbody.AddForce(Vector3.back * hoverForce);
-                    break;
-                }
-            case 3:
-                {
-                    if (other.attachedRigidbody)
-                        other.attachedRigidbody.AddForce(Vector3.right * hoverForce);
-                    break;
-                }
-                
-            case 4:
-                {
-                    if (other.attachedRigidbody)
-                        other.attachedRigidbody.AddForce(Vector3.left * hoverForce);
-                    break;
-                }
-                
+            Vector3 vector = Vector3.forward; // default
+            switch (napravlen)
+            {
+                case 1:
+                    {
+                        vector = Vector3.forward;
+                        break;
+                    }
+                case 2:
+                    {
+                        vector = Vector3.back;
+                        break;
+                    }
+                case 3:
+                    {
+                        vector = Vector3.right;
+                        break;
+                    }
+                case 4:
+                    {
+                        vector = Vector3.left;
+                        break;
+                    }
+
+            }
+            if (other.attachedRigidbody)
+            {
+                other.attachedRigidbody.AddForce(vector * hoverForce);
+            }
         }
-
-            
-        
-       
     }
-
-  
 }
